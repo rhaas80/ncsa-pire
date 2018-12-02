0740de204e2b563fc88653761e1f27c58810b011
Roland Haas <rhaas@tapir.caltech.edu>
2018-12-02 12:26:19 -0600

    bugfix fib example

--- examples/fib.c
+++ examples/fib.c
@@ -1,22 +1,28 @@
-int main(void) {
- int n = 10;
- #pragma omp parallel
- {
-  #pragma omp single
-  printf("fib(%d)=%d\n",n,fib(n));
- }
- return 0;
-}
+#include <stdio.h>
+#include <omp.h>
 
 int fib(int n) {
  int i,j;
  if(n<2) return n; 
  else {
-  #pragma omp task firstprivate(n)
+  #pragma omp task shared(i) firstprivate(n)
   i=fib(n-1);
-  #pragma omp task firstprivate(n)
-  j=fib(n-2)
+  #pragma omp task shared(j) firstprivate(n)
+  j=fib(n-2);
   #pragma omp taskwait
-  return i+j
+  return i+j;
  }
+}
+
+int main(void) {
+ int n = 35;
+ double start = omp_get_wtime();
+ #pragma omp parallel
+ {
+  #pragma omp single
+  printf("fib(%d)=%d\n",n,fib(n));
+ }
+ double end = omp_get_wtime();
+ printf("Took %g ms\n", (end-start)*1e3);
+ return 0;
 }
